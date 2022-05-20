32e5179f047c00e2027fdda42932d9c556151ac8
Maria <mrmejia@scl.edu.gt>
2022-05-20 07:10:11 -0600

    Se agrega programacion en Javascript, a ventana y menu del sistema operativo WIN.OS

--- js/main.js
+++ js/main.js
@@ -3,12 +3,30 @@
 //DEL DOCUMENTO OBTEN EL ELEMENTO MEDIANTE SU ID
 //ALMACENAR LA INFORMACIÓN EN VARIABLES.
 var menu = document.getElementById('menu');
-var icon = document.getElementById('icon');
+var icon_menu = document.getElementById('icon_menu');
+
+var ventana = document.getElementById('ventana');
+var icon_carpeta = document.getElementById('icon_carpeta');
+
+/*var cerrar = document.getElementById('cerrar');*/
+var menu2 = document.getElementById('menu');
+var icon_cerrar = document.getElementById('icon_cerrar')
 
 //FUNCIÓN ----- TRABAJAR CON EL DOM
 function activar(){
-    menu.classList.add('menu');
+    menu.classList.add("activar");
+}
+function activar2(){
+    ventana.classList.add("activar");
+}
+function desactivar(){
+    menu2.classList.remove("desactivar");
 }
 
+
 //EVENTO
-icon.addEventListener('click', activar);
+icon_menu.addEventListener('click', activar);
+icon_carpeta.addEventListener('click', activar2);
+icon_cerrar.addEventListener('click', desactivar);
+
+
